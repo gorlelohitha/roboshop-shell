3777f29d6861f4d78e8b3f2caab515332ea35efd
lohitha <gorlelohitha@gmail.com>
2023-07-20 15:38:41 +0530

    Roboshop scripting update DNS Record

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,4 @@
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 ## update listen address from 127.0.0.1 to 0.0.0.0
 systemctl enable mongod
