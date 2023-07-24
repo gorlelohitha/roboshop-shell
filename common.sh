5b793f2dbc3b4e9d7288819497435d0fd24fe2c4
lohitha <gorlelohitha@gmail.com>
2023-07-24 15:42:56 +0530

    functions

--- common.sh
+++ common.sh
@@ -2,8 +2,6 @@ func_nodejs()
 {
 log=/tmp/roboshop.log
 echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
-
-
 cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
  echo ">>>>>>>>>>> create Mongodb Repo <<<<<<<<"
 
@@ -47,6 +45,58 @@ echo ">>>>>>>>>>> Start User Service <<<<<<<<"
 systemctl daemon-reload &>>${log}
 systemctl enable ${component} &>>${log}
 systemctl restart ${component} &>>${log}
+}
+
+
+
+
+
+func-maven()
+{
+  echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+cp ${component}.service /etc/systemd/system/${component}.service
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+yum install maven -y
+
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+useradd roboshop
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+echo ">>>>>>>>>>> delete${component} service <<<<<<<<"
+rm -rf  /app
+
+mkdir /app
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+cd /app
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+unzip /tmp/${component}.zip
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+cd /app
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+mvn clean package
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+mv target/${component}-1.0.jar ${component}.jar
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+yum install mysql -y
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+mysql -h mysql.devopsb17.online -uroot -pRoboShop@1 < /app/schema/${component}.sql
+echo ">>>>>>>>>>> create ${component} service <<<<<<<<"
+
+systemctl enable ${component}
+systemctl restart ${component}
 }
 
 
